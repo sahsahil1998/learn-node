48b382adf2377d0147a034f112952e27ef031a44
sahsahil1998 <sahsahil1997@gmail.com>
2023-11-02 18:28:58 -0400

    pr 2

--- basics/foo.js
+++ basics/foo.js
@@ -1,7 +1,8 @@
 const args = process.argv;
-if (args.length > 0) {
-    args.forEach((v, i) => console.log(`${v}`));
-}
-else {
+if (args.length > 2) {
+    args.slice(2).forEach((arg) => {
+        console.log(arg);
+    });
+} else {
     console.log('Args not found');
 }
