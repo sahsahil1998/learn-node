d63e212142320a07134f4896e03bc98a3678f942
sahsahil1998 <sahsahil1997@gmail.com>
2023-11-02 18:55:50 -0400

    syncdelete files

--- operations/syncdelete.js
+++ operations/syncdelete.js
@@ -1,7 +1,15 @@
 const fs = require("fs");
 fs.readFile("./operations/data.txt", (err, data) => {
-  if (err) console.log(err.message);
-  else console.log(data);
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(data);
+    // Only delete the file after it has been read
+    try {
+      fs.unlinkSync("./operations/data.txt"); // delete the file
+      console.log("File deleted successfully.");
+    } catch (deleteErr) {
+      console.log(deleteErr.message);
+    }
+  }
 });
-fs.unlinkSync("./operations/data.txt");		// delete the file
-
